901020b1e58a4a4cd4e905a6432ee052a0d92305
Jørgen Aleksander Fagervik <jorgenafagervik@gmail.com>
2025-04-07 17:54:13 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This assumes you have installed wget, ts, and neovim already.
+# Use Neovim 0.11
+
+wget github.com/Jafagervik/nvim-config.git
+
+ln -sf nvim-config/ ~/.config/nvim
